b95a47071478ae4e04b20cf9aefcd98885a06fcd
s20001 <s20001@std.it-college.ac.jp>
2021-11-08 10:09:46 +0900

    Initial commit

---
+++ app/src/main/java/jp/ac/it_college/std/s20001/muscletraining/MainActivity.kt
@@ -0,0 +1,14 @@
+package jp.ac.it_college.std.s20001.muscletraining
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import jp.ac.it_college.std.s20001.muscletraining.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
