6f58d690bd74957661c8e53bc8a52f50839e2020
s20001 <s20001@std.it-college.ac.jp>
2021-11-22 12:18:56 +0900

    commit 2

--- app/src/main/java/jp/ac/it_college/std/s20001/muscletraining/MainActivity3.kt
+++ app/src/main/java/jp/ac/it_college/std/s20001/muscletraining/MainActivity3.kt
@@ -2,10 +2,13 @@ package jp.ac.it_college.std.s20001.muscletraining
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import jp.ac.it_college.std.s20001.muscletraining.databinding.ActivityMain3Binding
 
 class MainActivity3 : AppCompatActivity() {
+    private lateinit var binding : ActivityMain3Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main3)
+        binding = ActivityMain3Binding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/jp/ac/it_college/std/s20001/muscletraining/MainActivity4.kt
+++ app/src/main/java/jp/ac/it_college/std/s20001/muscletraining/MainActivity4.kt
@@ -2,10 +2,13 @@ package jp.ac.it_college.std.s20001.muscletraining
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import jp.ac.it_college.std.s20001.muscletraining.databinding.ActivityMain4Binding
 
 class MainActivity4 : AppCompatActivity() {
+    private lateinit var binding : ActivityMain4Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main4)
+        binding = ActivityMain4Binding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
