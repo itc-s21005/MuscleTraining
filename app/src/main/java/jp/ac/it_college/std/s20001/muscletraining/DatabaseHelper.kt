904ad3f0c38d8bed891d48a0f52abb54e28cc702
s20011 <s20011@std.it-college.ac.jp>
2022-02-07 11:30:56 +0900

    データベース

---
+++ app/src/main/java/jp/ac/it_college/std/s20001/muscletraining/DatabaseHelper.kt
@@ -0,0 +1,27 @@
+package jp.ac.it_college.std.s20001.muscletraining
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DatabaseHelper(context: Context): SQLiteOpenHelper(context, DATABASE_NAME,
+    null, DATABASE_VERSION) {
+
+    companion object{
+        private const val DATABASE_NAME = "record.db"
+        private const val DATABASE_VERSION = 1
+    }
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        val sb = """
+            CREATE TABLE record (
+            menu TEXT,
+            
+            )
+        """.trimIndent()
+
+        db!!.execSQL(sb)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {}
+}
