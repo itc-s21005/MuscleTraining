06b5113a2dbf7b2be088ded599e7fe8c1c88c405
s20011 <s20011@std.it-college.ac.jp>
2022-02-22 10:23:43 +0900

    筋トレ管理画面完成

--- app/src/main/java/jp/ac/it_college/std/s20001/muscletraining/RecordActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s20001/muscletraining/RecordActivity.kt
@@ -3,11 +3,13 @@ package jp.ac.it_college.std.s20001.muscletraining
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import jp.ac.it_college.std.s20001.muscletraining.databinding.ActivityRecordBinding
 
 class RecordActivity : AppCompatActivity() {
     private val helper = DatabaseHelper(this@RecordActivity)
     private lateinit var binding: ActivityRecordBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +17,51 @@ class RecordActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         Log.d("RecordActivity", "onCreate RecordActivity")
+
+        Log.d("RecordActivity", "records = ${selectDb()}")
+        val adapter = RecyclerAdapter(selectDb())
+        val manager = LinearLayoutManager(this@RecordActivity)
+        binding.records.let {
+            it.adapter = adapter
+            manager.orientation = LinearLayoutManager.VERTICAL
+            it.layoutManager = manager
+        }
+    }
+
+    private fun selectDb(): MutableList<MutableMap<String, String>>{
+
+        val records: MutableList<MutableMap<String, String>> = mutableListOf()
+        val db = helper.readableDatabase
+        val sqlCount = "SELECT COUNT(*) AS count FROM record"
+        val sqlSelect = "SELECT *  FROM record"
+        val cursor = db.rawQuery(sqlSelect, null)
+
+
+        /*if(cursor.moveToFirst()){
+            cursor.let {
+                val count = it.getColumnIndex("count").toInt()
+                Log.d("RecordActivity", "count = ${count}")
+            }
+            cursor.close()
+        }*/
+
+        while(cursor.moveToNext()) {
+            cursor.let {
+                val menuIndex = it.getColumnIndex("menu")
+                val dateIndex = it.getColumnIndex("date")
+                records.add(
+                    mutableMapOf(
+                        "menu" to it.getString(menuIndex),
+                        "date" to it.getString(dateIndex)
+                    )
+                )
+            }
+
+        }
+
+        cursor.close()
+
+        return records
+
     }
 }
---
+++ app/src/main/java/jp/ac/it_college/std/s20001/muscletraining/RecyclerAdapter.kt
@@ -0,0 +1,30 @@
+package jp.ac.it_college.std.s20001.muscletraining
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerAdapter(private val record: MutableList<MutableMap<String, String>>)
+    : RecyclerView.Adapter<RecyclerAdapter.ViewHolder>() {
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val menuText: TextView = view.findViewById(R.id.menu)
+        val dateText: TextView = view.findViewById(R.id.date)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.row, parent, false)
+        val holder = ViewHolder(view)
+
+        return holder
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.menuText.text = record[position]["menu"]
+        holder.dateText.text = record[position]["date"]
+    }
+
+    override fun getItemCount() = record.size
+}
