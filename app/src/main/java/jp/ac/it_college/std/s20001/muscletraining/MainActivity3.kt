439e02bd1614ba11a402c0a693b1c57fa0860490
s20011 <s20011@std.it-college.ac.jp>
2022-02-22 10:24:13 +0900

    データベース追加

--- app/src/main/java/jp/ac/it_college/std/s20001/muscletraining/MainActivity3.kt
+++ app/src/main/java/jp/ac/it_college/std/s20001/muscletraining/MainActivity3.kt
@@ -9,12 +9,16 @@ import android.widget.SimpleAdapter
 import jp.ac.it_college.std.s20001.muscletraining.databinding.ActivityMain3Binding
 import org.json.JSONArray
 import org.json.JSONObject
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity3 : AppCompatActivity() {
     private lateinit var binding : ActivityMain3Binding
     private val _list: MutableList<MutableMap<String, String>> = mutableListOf()
     private val descriptions: ArrayList<JSONArray> = arrayListOf()
     private val images: ArrayList<String> = arrayListOf()
+    private val helper = DatabaseHelper(this@MainActivity3)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMain3Binding.inflate(layoutInflater)
@@ -31,12 +35,16 @@ class MainActivity3 : AppCompatActivity() {
         muscleList.adapter = adapter
         muscleList.setOnItemClickListener { _,_, position, _ ->
 
+            val name = _list[position]["name"].toString()
             val description = arrayListOf<String>()
             val image = images[position]
             for(i in 0 until descriptions[position].length()) {
                 description.add(descriptions[position][i].toString())
             }
 
+            insertSpl(name)
+
+            Log.d("Main3", "name = $name")
             Log.d("Main3", "description = $description")
             Log.d("Main3", "image = $image")
             val MainActivity4 = Intent(this@MainActivity3, MainActivity4::class.java)
@@ -73,5 +81,23 @@ class MainActivity3 : AppCompatActivity() {
             Log.d("MainActivity3", "description = ${description[0]}")
 
         }
+    }
+
+    private fun insertSpl(menu: String){
+        Log.d("MainActivity3", "start insertSpl")
+        val db = helper.writableDatabase
+        val insert = """INSERT INTO record (
+            menu, date
+            ) VALUES (?,?)""".trimIndent()
+        val stmt = db.compileStatement(insert)
+        stmt.bindString(1, menu)
+        stmt.bindString(2, getDate())
+        stmt.executeInsert()
+    }
+
+    private fun getDate():String{
+        val date = Date()
+        val df = SimpleDateFormat("yyyy/MM/dd")
+        return df.format(date).toString()
     }
 }
